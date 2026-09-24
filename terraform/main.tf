400fffac499a3b5051198dc6d1dfecb2dc692c89
JescAude18 <jessicamoussougan@gmail.com>
2026-09-24 01:15:35 +0100

    feat :wrench: : added terraform code to create the VM instance

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+resource "oci_core_instance" "roadmapsh_vm2" {
+    compartment_id = var.compartment_ocid
+    availability_domain = data.oci_identity_availability_domain.ad.name
+    display_name = "roadmapsh_vm2"
+    shape = var.shape
+    
+    create_vnic_details {
+        subnet_id        = var.subnet_ocid
+        display_name     = "roadmapsh_vnic2"
+        assign_public_ip = true
+    }
+
+    source_details {
+        source_type = "image"
+        source_id   = var.image_ocid
+    }
+
+    metadata = {
+        ssh_public_key = var.ssh_public_key
+    }
+}
