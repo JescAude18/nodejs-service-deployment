61b531de974f24dc3ec6b013f3d01cd45658e678
JescAude18 <jessicamoussougan@gmail.com>
2026-09-24 01:14:14 +0100

    feat :sparkles: : added vm_shape, subnet_ocid, image_ocid and ssh_public_key variables to create the VM

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,10 @@ variable "tenancy_ocid" {
     description = "OCID of the OCI tenancy"    
 }
 
+variable "compartment_ocid" {
+    type        = string
+    description = "OCID of the compartment where resources will be created"
+}
 variable "user_ocid" {
     type        = string
     description = "OCID of the OCI user"
@@ -21,4 +25,24 @@ variable "private_key_path" {
 variable "region" {
     type        = string
     description = "OCI region where resources will be created"
+}
+
+variable "shape" {
+    type        = string
+    description = "VM instance shape"
+}
+
+variable "subnet_ocid" {
+    type        = string
+    description = "OCID of the existing subnet where the VM will be created"
+}
+
+variable "image_ocid" {
+    type        = string
+    description = "OCID of the image used for VM creation"
+}
+
+variable "ssh_public_key" {
+    type        = string
+    description = "Public SSH key used to access the instance"
 }
